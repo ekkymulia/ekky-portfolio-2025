7874864c57b8893ca72f7e4929cfeaf4cc8beb2a
ekkymulia <ekky.mulialasardi@gmail.com>
2025-03-02 15:11:11 +0900

    test key

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Footer from "@/components/website/Footer";
 import { MorphingText } from "@/components/magicui/morphing-text";
 
 export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_OPENAI_API_KEY)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-16 sm:p-16 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
